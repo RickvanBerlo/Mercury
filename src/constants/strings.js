179c2e0ad4ad7e972da033c14147511c664a67b5
rick@bluenotion.nl <rickvanberlo@pop-os.localdomain>
2020-01-29 20:20:20 +0100

    spelling check

--- src/constants/strings.js
+++ src/constants/strings.js
@@ -22,13 +22,13 @@ const strings = {
     ABOUT: {
         TITLE: "Over mij",
         DESCRIPTION:
-            `De ICT sector heeft mij van kleins af aan geïnteresseerd. Ik zag hoe ICT de wereld aan het veranderen was en wou hier graag een steentje aan bijdragen. Met deze motivatie ben ik in de ICT sector gedoken.
+            `De ICT-sector heeft mij van kleins af aan geïnteresseerd. Ik zag hoe ICT de wereld aan het veranderen was en  ik wilde hier graag een steentje aan bijdragen. Met deze motivatie ben ik in de ICT sector gedoken.
 
             Als eerste heb ik een systeembeheerders opleiding gevolgd in Nijmegen. Hierin heb ik vaardigheden geleerd zoals: Het configureren en beheren van servers, omgaan met klantencontact en het configureren van netwerken.
-            Tijdens deze opleiding heb ik ook mijn eerste ervaring gehad met programmeren. Door deze ervaring wist ik al snel dat ik een programmeur wilde worden en ben na het afronden van deze opleiding een HBO informatica opleiding gestart.
+            Tijdens deze opleiding heb ik ook mijn eerste ervaring gehad met programmeren. Door deze ervaring wist ik al snel dat ik een programmeur wilde worden en ben ik na het afronden van deze opleiding een HBO informatica opleiding gestart.
             
-            Hierin heb ik de benodigde vaardigheden geleerd om een programmeur te worden. Enkele van deze vaardigheden zijn: Het programmeren in verschillende programmeertalen, Het in gebruik kunnen nemen van verschillende design patterns en het uitdenken van complexe architecturen.
-            Om mijn kennis te verrijken heb ik als minor software architectuur gevolgd op Avans. Hierin heb ik kennis gemaakt met de programmeertaal C++.
+            Hierin heb ik de benodigde vaardigheden geleerd om programmeur te worden. Enkele van deze vaardigheden zijn: Het programmeren in verschillende programmeertalen, het in gebruik kunnen nemen van verschillende design patterns en het uitdenken van complexe architecturen.
+            Om mijn kennis te verrijken heb ik als minor software architectuur gevolgd op Avans. Hier heb ik kennis gemaakt met de programmeertaal C++.
 
             Naast de opleidingen ben ik ook thuis bezig met het verrijken van mijn kennis. Dit komt omdat ik leergierig en zeer geïnteresseerd ben in nieuwe technologieën.
 
@@ -42,8 +42,8 @@ const strings = {
                 SUBTITLE: "Thuis service medewerker",
                 DATE: "Juni 2017 / Juni 2019",
                 DESCRIPTION:
-                    `Tijdens deze baan help je klanten van Student aan Huis met hun computer problemen.
-                    geen enkele afspraak zal hetzelfde zijn waardoor je als werknemer zeer vindingrijk moet zijn om alle problemen op te kunnen lossen.`,
+                    `Tijdens deze baan help je klanten van Student aan Huis met computer problemen.
+                    Geen enkele afspraak is hetzelfde waardoor je als werknemer zeer vindingrijk moet zijn om alle problemen op te kunnen lossen.`,
             },
         ]
     },
@@ -56,14 +56,14 @@ const strings = {
                 DATE: "September 2016 / heden",
                 DESCRIPTION:
                     `De opleiding HBO informatica heeft me geholpen bij het leren van verschillende programmeertalen zoals: C#, Java, Javascript en SQL. 
-                    Om mijn kennis van programmeertalen te verrijken heb ik als minor software architectuur bij Avans gekozen. Hier heb ik kennis gemaakt met de programmeertaal C++ en verschillende design patterns.`,
+                    Om mijn kennis van programmeertalen te verrijken heb ik de minor software architectuur bij Avans gekozen. Hier heb ik kennis gemaakt met de programmeertaal C++ en verschillende design patterns.`,
             },
             {
                 TITLE: "MBO Systeemheerder",
                 SUBTITLE: "Netwerkbeheerder",
                 DATE: "September 2013 / Juni 2016",
                 DESCRIPTION:
-                    `De opleiding MBO systeembeheerder heeft me laten zien hoe interessant de ICT sector is. 
+                    `De opleiding MBO systeembeheerder heeft mij laten zien hoe interessant de ICT sector is. 
                     Tijdens deze opleiding leerde we servers beheren en configureren, netwerken aanleggen en programmeren in de taal Java.`,
             },
         ]
@@ -77,7 +77,7 @@ const strings = {
                 DATE: "September-2019 / Heden",
                 DESCRIPTION:
                     `Bij Bluenotion heb ik mijn afstudeerstage van het HBO volbracht. 
-                    Als afstudeeropdracht heb ik samen met mijn afstudeerpartner een app gemaakt voor een extern partij. De app geeft het bedrijf de mogelijkheid om een plattegrond te creëren van een desbetreffende ruimte. Deze functionaliteit is gemaakt met behulp van Augmented Reality.`
+                    Als afstudeeropdracht heb ik samen met mijn afstudeerpartner een app gemaakt voor een externe partij. De app geeft het bedrijf de mogelijkheid om een plattegrond te creëren van een desbetreffende ruimte. Deze functionaliteit is gemaakt met behulp van Augmented Reality.`
             },
             {
                 TITLE: "Cloudy Hostings",
@@ -102,7 +102,7 @@ const strings = {
                 TITLE: "Mercury",
                 SUBTITLE: "React",
                 DESCRIPTION:
-                    `In het project Mercury wordt gewerkt aan mijn eigen persoonlijke assistente. Hierin zal ik mijn eigen agenda bij kunnen houden, bestanden naar toe kunnen uploaden en email me kunnen ontvangen/verzenden.
+                    `In het project Mercury wordt gewerkt aan mijn eigen persoonlijke assistente. Hierin zal ik mijn eigen agenda bij kunnen houden, bestanden naar toe kunnen uploaden en e-mail mee kunnen ontvangen of verzenden.
                     `,
                 LINKS:
                     [
