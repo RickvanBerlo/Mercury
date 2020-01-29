c6bc59d0c34e0e5f61a9d01531128fed02cf4cc1
rick@bluenotion.nl <rickvanberlo@pop-os.localdomain>
2020-01-29 16:41:49 +0100

    updates for live version

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ ReactDOM.render(<PersonalPage />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.l your server page, add this liney/CRA-PWA
 serviceWorker.unregister();
