8486e802bcbeb34d5819b97296c4636a06043b6d
rick@bluenotion.nl <rickvanberlo@pop-os.localdomain>
2020-02-05 18:52:38 +0100

    added parralaxHeader

--- src/components/headers/parallaxHeader.js
+++ src/components/headers/parallaxHeader.js
@@ -2,13 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import IconButton from '../buttons/iconButton';
 import colors from '../../constants/colors';
-import background from '../../assets/background.jpg';
+import background from '../../assets/background.jpg'
 import backgroundMobile from '../../assets/backgroundmobile.jpg';
+import { ParallaxBanner } from 'react-scroll-parallax';
+import LayerOne from '../../assets/LayerOne.png';
+import LayerTwo from '../../assets/LayerTwo.png';
+import LayerThree from '../../assets/LayerThree.png';
+import LayerFour from '../../assets/LayerFour.png';
+import LayerFive from '../../assets/LayerFive.png';
+import { mobilecheck } from '../../utils/deviceCheck';
 
 const Header = ({ name, links, nextSection }) => {
+    var check = mobilecheck();
     return (
         <Container>
-            <BackgroundContainer></BackgroundContainer>
+            {!check ? <CustomParallaxBanner className="your-class"
+                layers={[
+                    {
+                        children: <ParallaxImage image={LayerFive} />,
+                        amount: 0.4,
+                        props: { style: { top: "0%" } }
+                    },
+                    {
+                        children: <ParallaxImage image={LayerFour} />,
+                        amount: 0.3,
+                        props: { style: { top: "0%" } }
+                    },
+                    {
+                        children: <ParallaxImage image={LayerThree} />,
+                        amount: 0.2,
+                        props: { style: { top: "0%" } }
+                    },
+                    {
+                        children: <ParallaxImage image={LayerTwo} />,
+                        amount: 0.1,
+                        props: { style: { top: "0%" } }
+                    },
+                    {
+                        children: <ParallaxImage image={LayerOne} />,
+                        amount: 0.0,
+                        props: { style: { top: "0%" } }
+                    }
+                ]} />
+                :
+                <BackgroundMobileContainer />
+            }
             <CenterContainer>
                 <Title>{name}</Title>
                 {links.map((link, index) => {
@@ -18,7 +56,7 @@ const Header = ({ name, links, nextSection }) => {
             <NavContainer>
                 <IconButton icon={['fas', 'arrow-alt-circle-down']} size="3x" color="white" onClick={() => { window.scrollTo(0, window.scrollY + document.getElementById(nextSection).getBoundingClientRect().y + 4); }} />
             </NavContainer>
-        </Container>
+        </Container >
     );
 }
 
@@ -26,28 +64,45 @@ const Header = ({ name, links, nextSection }) => {
 const Container = styled.div`
 height: 100vh;
 width: 100vw;
-background-image: linear-gradient(${colors.HEADER_BACKGROUND_COLOR} 80%, ${colors.DARK_GRAY} 1%, ${colors.DARK_GRAY});
+background-color: ${colors.HEADER_BACKGROUND_COLOR};
+`
+//background-image: linear-gradient(${colors.HEADER_BACKGROUND_COLOR} 80%, ${colors.DARK_GRAY} 1%, ${colors.DARK_GRAY});
+const CustomParallaxBanner = styled(ParallaxBanner)`
+    height: 100vh !important;
 `
 
-const BackgroundContainer = styled.div`
-position: absolute;
-top: 0;
-right: 0;
-bottom: 0px;
-left: 0;
-box-sizing: border-box;
-background: url(${background}) bottom;
-background-size: 100vw;
-background-repeat: no-repeat;
-@media (max-width: 767px) {
-    background: url(${backgroundMobile}) center;
-    background-size: cover;
-    @media (max-height: 467px) {
-        background: url(${background}) bottom;
-        background-size: 100vw;
-        background-repeat: no-repeat;
+const ParallaxImage = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: -1px;
+    left: 0;
+    box-sizing: border-box;
+    background: url(${props => props.image}) bottom;
+    background-size: 100vw;
+    background-repeat: no-repeat;
+`
+
+const BackgroundMobileContainer = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: -60px;
+    left: 0;
+    background: url(${background}) bottom;
+    background-size: 100vw;
+    background-repeat: no-repeat;
+    @media (max-width: 767px) {
+        background: url(${backgroundMobile}) center;
+        background-size: cover;
+        @media (max-height: 467px) {
+            background: url(${background}) bottom;
+            background-size: 100vw;
+            background-repeat: no-repeat;
+        }
     }
-}
 `
 
 const NavContainer = styled.div`
--- src/pages/personalPage.js
+++ src/pages/personalPage.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
-import styled from 'styled-components';
-import Header from '../components/headers/staticHeader';
+import Header from '../components/headers/parallaxHeader';
 import AboutMeSection from '../components/personalPageSections/aboutMeSection';
 import ContactSection from '../components/personalPageSections/contactSection';
 import Footer from '../components/footers/personalPageFooter';
@@ -11,6 +10,7 @@ import TopNavbar from '../components/navbars/topNavbar';
 import CurriculemVitaeSection from '../components/personalPageSections/curriculemVitaeSection';
 import background from '../assets/background.jpg';
 import backgroundMobile from '../assets/backgroundmobile.jpg';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 const CurriculemVitae = () => {
   useEffect(() => {
@@ -18,20 +18,15 @@ const CurriculemVitae = () => {
   }, []);
 
   return (
-    <Container id="rootContainer">
+    <ParallaxProvider>
       <TopNavbar buttons={strings.NAVIGATION} />
       <Header name={strings.NAME} links={strings.WEBLINKS} nextSection={strings.NAVIGATION[1].PRIVATE_NAME} backgroundImage={background} mobileBackgroundImage={backgroundMobile} />
       <AboutMeSection content={strings.ABOUT} name={strings.NAVIGATION[1].PRIVATE_NAME} infoTitle={strings.GENERAL_INFORMATION} infoContent={strings.GENERAL_INFORMATION_DESCRIPTION} />
       <CurriculemVitaeSection name={strings.NAVIGATION[2].PRIVATE_NAME} />
       <ContactSection name={strings.NAVIGATION[3].PRIVATE_NAME} content={strings.CONTACT} contactTitle={strings.CONTACT_TITLE} contactContent={strings.CONTACT_DESCRIPTION} recaptchaConfig={config.RECAPTCHA} />
       <Footer links={strings.WEBLINKS} />
-    </Container>
+    </ParallaxProvider>
   );
 }
-
-//styles
-const Container = styled.div`
-    flex: 1;
-  `
 
 export default CurriculemVitae;
