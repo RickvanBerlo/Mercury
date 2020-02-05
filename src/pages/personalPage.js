32140fb673a75f2a830bffcef278e5f441404b44
rick@bluenotion.nl <rickvanberlo@pop-os.localdomain>
2020-02-05 11:49:19 +0100

    begin of parralax scrolling + dependency injection static headere

--- src/pages/personalPage.js
+++ src/pages/personalPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import Header from '../components/headers/personalPageHeader';
+import Header from '../components/headers/staticHeader';
 import AboutMeSection from '../components/personalPageSections/aboutMeSection';
 import ContactSection from '../components/personalPageSections/contactSection';
 import Footer from '../components/footers/personalPageFooter';
@@ -9,6 +9,8 @@ import { loadReCaptcha } from 'react-recaptcha-google';
 import config from '../constants/config';
 import TopNavbar from '../components/navbars/topNavbar';
 import CurriculemVitaeSection from '../components/personalPageSections/curriculemVitaeSection';
+import background from '../assets/background.jpg';
+import backgroundMobile from '../assets/backgroundmobile.jpg';
 
 const CurriculemVitae = () => {
   useEffect(() => {
@@ -18,7 +20,7 @@ const CurriculemVitae = () => {
   return (
     <Container id="rootContainer">
       <TopNavbar buttons={strings.NAVIGATION} />
-      <Header name={strings.NAME} links={strings.WEBLINKS} nextSection={strings.NAVIGATION[1].PRIVATE_NAME} />
+      <Header name={strings.NAME} links={strings.WEBLINKS} nextSection={strings.NAVIGATION[1].PRIVATE_NAME} backgroundImage={background} mobileBackgroundImage={backgroundMobile} />
       <AboutMeSection content={strings.ABOUT} name={strings.NAVIGATION[1].PRIVATE_NAME} infoTitle={strings.GENERAL_INFORMATION} infoContent={strings.GENERAL_INFORMATION_DESCRIPTION} />
       <CurriculemVitaeSection name={strings.NAVIGATION[2].PRIVATE_NAME} />
       <ContactSection name={strings.NAVIGATION[3].PRIVATE_NAME} content={strings.CONTACT} contactTitle={strings.CONTACT_TITLE} contactContent={strings.CONTACT_DESCRIPTION} recaptchaConfig={config.RECAPTCHA} />
