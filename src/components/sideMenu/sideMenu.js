8452ee6ddbdec7e83bf8cfe4fcbabc3d3a6c9ec7
rick@bluenotion.nl <rickvanberlo@pop-os.localdomain>
2020-02-20 16:19:07 +0100

    refactor side Menu animations

--- src/components/sideMenu/sideMenu.js
+++ src/components/sideMenu/sideMenu.js
@@ -1,17 +1,74 @@
-import React, { useState, memo } from "react";
+import React, { memo, useEffect } from "react";
 import styled from 'styled-components';
 import colors from '../../constants/colors';
+import getScreenResolution from '../../utils/screenResolution';
+
+const INIT_LABEL_Y = 85;
+const INIT_SIDEMENU_X = -300;
 
 const SideMenu = ({ setCurrentPage }) => {
-    const [show, setShow] = useState(false);
-    const items = ["Agenda"];
+    let drag = false;
+    let labelY = INIT_LABEL_Y;
+    let sidemenuX = INIT_SIDEMENU_X;
+    let mouseY = null;
+    let mouseX = null;
+    let screenHeight = getScreenResolution().height;
+
+    const setOffset = (event) => {
+        if (drag) {
+            const offsetY = mouseY - labelY;
+            const offsetX = mouseX - sidemenuX;
+            const Y = event.pageY - offsetY;
+            const X = event.pageX - offsetX;
+
+            document.getElementById("label").style.cursor = "grab";
+            document.getElementById("label").style.top = Y < 85 ? 85 : Y > screenHeight - 75 ? screenHeight - 75 : Y + "px";
+            document.getElementById("sideMenu").style.left = X > 0 ? 0 : X < -300 ? -300 : X + "px";
+            document.getElementById("sideMenu").style.transition = "none";
+            ToggleAnimationLabel(X > 0 ? true : false);
+        }
+    }
+
+    const startDrag = (event) => {
+        mouseY = event.pageY;
+        mouseX = event.pageX;
+        drag = true
+    }
+
+    const endDrag = (event) => {
+        if (drag) {
+            const X = event.pageX - (mouseX - sidemenuX);
+
+            drag = false;
+            sidemenuX = X >= -150 ? 0 : -300;
+            labelY = (event.pageY - (mouseY - labelY));
+            document.getElementById("label").style.cursor = "pointer";
+            document.getElementById("sideMenu").style.left = X >= -150 ? 0 : -300 + "px";
+            document.getElementById("sideMenu").style.transition = "left 0.4s linear";
+            ToggleAnimationLabel(sidemenuX === 0 ? true : false);
+        }
+    }
+
+    const onClick = (event) => {
+        if (event.pageX === mouseX) {
+            sidemenuX = sidemenuX === 0 ? -300 : 0;
+            document.getElementById("sideMenu").style.left = sidemenuX >= -150 ? 0 : -300 + "px";
+            ToggleAnimationLabel(sidemenuX === 0 ? true : false);
+        }
+    }
+
+    useEffect(() => {
+        document.getElementById("label").addEventListener("mousedown", startDrag, false);
+        window.addEventListener("mouseup", endDrag, false);
+        window.addEventListener("mousemove", setOffset, false);
+    }, [])
 
     return (
-        <Container show={show}>
-            <Label onClick={() => { setShow(!show) }} show={show}>
-                <Bar1 show={show} />
-                <Bar2 show={show} />
-                <Bar3 show={show} />
+        <Container id="sideMenu" offsetX={INIT_SIDEMENU_X}>
+            <Label id="label" onClick={onClick} top={INIT_LABEL_Y}>
+                <Bar1 id="bar1" />
+                <Bar2 id="bar2" />
+                <Bar3 id="bar3" />
             </Label>
             <ContainerTitle>
                 <Title>Mercury</Title>
@@ -35,26 +92,30 @@ const SideMenu = ({ setCurrentPage }) => {
                 <PageLink>Hsdsd</PageLink>
                 <PageLink>Hsdsd</PageLink>
                 <PageLink>Hsdsd</PageLink>
-
             </ContainerPages>
         </Container>
     )
+}
+
+const ToggleAnimationLabel = (toggle) => {
+    document.getElementById("bar1").style.transform = toggle ? "rotate(-45deg) translate(-9px, 6px)" : "rotate(0deg) translate(0px, 0px)";
+    document.getElementById("bar2").style.opacity = toggle ? 0 : 1;
+    document.getElementById("bar3").style.transform = toggle ? "rotate(45deg) translate(-9px, -7px)" : "rotate(0deg) translate(0px, 0px)";
 }
 
 const Container = styled.div`
     position: absolute;
     z-index: 10;
     top: 0;
-    left: 0;
+    left: ${props => props.offsetX}px;
     background-color: ${colors.WHITE};
     height: 100vh;
     width: 300px;
     box-shadow: 0px 0px 10px 2px;
-    transition: transform 0.3s linear;
-    transform: translateX(${props => props.show ? "0px" : "-300px"});
     border-top-right-radius: 10px;
     border-bottom-right-radius: 10px;
     text-align:center;
+    transition: left 0.4s linear;
 `
 
 const ContainerPages = styled.div`
@@ -93,7 +154,7 @@ const Label = styled.div`
     position: absolute;
     padding-top: 10px;
     padding-left: 10px;
-    top: 10%;
+    top: ${props => props.top}px;
     left: 300px;
     background-color: white;
     height: 50px;
@@ -112,7 +173,6 @@ const Bar1 = styled.div`
     background-color: #333;
     margin: 6px 0;
     transition: transform 0.3s linear;
-    transform: rotate(${props => props.show ? "-45deg" : "0deg"}) translate(${props => props.show ? "-9px, 6px" : "0px 0px"});
 `
 const Bar2 = styled.div`
     width: 30px;
@@ -120,7 +180,6 @@ const Bar2 = styled.div`
     background-color: #333;
     margin: 6px 0;
     transition: opacity 0.3s linear;
-    opacity: ${props => props.show ? 0 : 1};
 `
 const Bar3 = styled.div`
     width: 30px;
@@ -128,7 +187,6 @@ const Bar3 = styled.div`
     background-color: #333;
     margin: 6px 0;
     transition: transform 0.3s linear;
-    transform: rotate(${props => props.show ? "45deg" : "0deg"}) translate(${props => props.show ? "-9px, -7px" : "0px 0px"});
 `
 
 const AreEqual = (prevProps, nextProps) => {
--- src/utils/lerp.js
+++ src/utils/lerp.js
@@ -1,7 +1,9 @@
 export const lerp = (begin, end, amount) => {
+    //amount = percentage
     return begin * (1 - amount) + end * amount;
 };
 
 export const invlerp = (begin, end, amount) => {
+    //amount is value between begin and end.
     return (amount - begin) / (end - begin)
 }
