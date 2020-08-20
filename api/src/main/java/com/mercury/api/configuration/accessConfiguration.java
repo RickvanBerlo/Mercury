3c4dfe5fd379469e225af66d30717d840b416b55
Rick van Berlo <rick.van.berlo@first8.nl>
2020-08-20 13:14:45 +0200

    added oauth2 security

--- api/src/main/java/com/mercury/api/configuration/accessConfiguration.java
+++ api/src/main/java/com/mercury/api/configuration/accessConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class accessConfiguration {
+public class AccessConfiguration {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
