d8b7526d818ddab94f66838786706d3e95d2baa9
Rick van Berlo <rick.van.berlo@first8.nl>
2020-08-24 11:10:42 +0200

    working custom oauth2 login and approval

---
+++ mercury_security/src/main/java/com/mercury/authentication/mercury_security/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.mercury.authentication.mercury_security.controller;
+
+import java.util.TreeMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.AuthorizationRequest;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes(types = AuthorizationRequest.class)
+public class AuthController {
+    
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @RequestMapping("/oauth/confirm_access")
+    public ModelAndView getAccessConfirmation(@ModelAttribute AuthorizationRequest clientAuth) throws Exception {
+        ClientDetails client = clientDetailsService.loadClientByClientId(clientAuth.getClientId());
+        TreeMap<String, Object> model = new TreeMap<String, Object>();
+        model.put("auth_request", clientAuth);
+        model.put("client", client);
+        return new ModelAndView("authorize", model);
+    }
+}
