816349de833c11fea4a19af89d6cb0431713d216
Rick van Berlo <rick.van.berlo@first8.nl>
2020-09-03 15:52:54 +0200

    added optimizations

--- src/constants/pages.js
+++ src/constants/pages.js
@@ -1,15 +1,4 @@
-import HomePage from '../pages/homePage';
-import CalendarPage from '../pages/calendarPage';
-import NotesPage from '../pages/notesPage';
-import StoragePage from '../pages/storagePage';
-//import ScrumboardsPage from '../pages/scrumboardsPage';
-//import BillsPage from '../pages/billsPage';
-import ConfigPage from '../pages/configPage';
-import DayPage from '../pages/dayPage';
-import EventEditPage from '../pages/eventEditPage';
-import EventPage from '../pages/eventPage';
-import NoteEditPage from '../pages/noteEditPage';
-
+import { lazy } from 'react';
 import CalanderIcon from 'react-ionicons/lib/MdCalendar';
 import HomeIcon from 'react-ionicons/lib/MdHome';
 import NotesIcon from 'react-ionicons/lib/MdList';
@@ -17,6 +6,17 @@ import StorageIcon from 'react-ionicons/lib/MdFolder';
 //import ScrumboardsIcon from 'react-ionicons/lib/MdClipboard';
 //import BillsIcon from 'react-ionicons/lib/MdCash';
 import ConfigIcon from 'react-ionicons/lib/MdBuild';
+//import ScrumboardsPage from '../pages/scrumboardsPage';
+//import BillsPage from '../pages/billsPage';
+const HomePage = lazy(() => import('../pages/homePage'));
+const CalendarPage = lazy(() => import('../pages/calendarPage'));
+const NotesPage = lazy(() => import('../pages/notesPage'));
+const StoragePage = lazy(() => import('../pages/storagePage'));
+const ConfigPage = lazy(() => import('../pages/configPage'));
+const DayPage = lazy(() => import('../pages/dayPage'));
+const EventEditPage = lazy(() => import('../pages/eventEditPage'));
+const EventPage = lazy(() => import('../pages/eventPage'));
+const NoteEditPage = lazy(() => import('../pages/noteEditPage'));
 
 export const pageNames = {
     HOME: "Home",
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {Suspense, lazy} from "react";
+import styled from 'styled-components';
 import { Switch } from "react-router-dom";
 import PrivateRoute from './privateRoute';
 import PublicRoute from './publicRoute';
+import background from '../assets/background.webp';
+import colors from '../constants/colors';
 
 //pages
-import Authentication from '../pages/authenticationPage';
-import Dashboard from '../pages/dashboardPage';
-import CurriculemVitae from "../pages/curriculemViteaPage";
+const Authentication = lazy(() => import('../pages/authenticationPage'));
+const Dashboard = lazy(() => import('../pages/dashboardPage'));
+const CurriculemVitae = lazy(() => import('../pages/curriculemViteaPage'));
 
 const Routes = () => {
     return (
+        <Suspense fallback={<Container background={background}></Container>}>
         <Switch>
             <PublicRoute exact path="/" component={CurriculemVitae} />
             <PublicRoute path="/login" render={(routeProps) => {
@@ -22,7 +26,20 @@ const Routes = () => {
                 return <Dashboard {...routeProps} />
             }} />
         </Switch>
+        </Suspense>
     );
 }
+
+const Container = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: url(${props => props.background}) bottom;
+    background-size: 100vw;
+    background-repeat: no-repeat;
+    background-color: ${colors.HEADER_BACKGROUND_COLOR};
+`
 
 export default Routes;
