83df40cbf407b8cebc0d3d9b75739726eabb4c80
Rick van Berlo <rick.van.berlo@first8.nl>
2020-09-11 10:03:36 +0200

    added test for preferences endpoints

---
+++ api/src/test/java/com/mercury/api/PreferenceControllerTests.java
@@ -0,0 +1,35 @@
+package com.mercury.api;
+
+import com.mercury.api.controller.PreferencesController;
+import com.mercury.api.model.preferences.Preferences;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestMethodOrder(OrderAnnotation.class)
+public class PreferenceControllerTests {
+    @Autowired
+    private PreferencesController preferencesController;
+
+    @Test
+    @Order(1)
+    public void getInitPreferences() {
+        Preferences pref = preferencesController.getPreferences().getBody();
+        Assertions.assertEquals(pref.isClock(), false);
+        Assertions.assertEquals(pref.isDarkmode(), false);
+    }
+
+    @Test
+    @Order(2)
+    public void PostPreferences() {
+        Preferences pref = preferencesController.savePreferences(new Preferences(true, true)).getBody();
+        Assertions.assertEquals(pref.isClock(), true);
+        Assertions.assertEquals(pref.isDarkmode(), true);
+    }
+}
